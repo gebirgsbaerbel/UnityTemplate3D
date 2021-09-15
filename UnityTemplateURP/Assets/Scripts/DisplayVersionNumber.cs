7b5a250f1ba5cd8cee06e55ac3711035ff8a9083
reichart.barbara@gmail.com <reichart.barbara@gmail.com>
2021-09-15 12:05:53 +0200

    Add script that logs version number on application start

---
+++ UnityTemplateURP/Assets/Scripts/DisplayVersionNumber.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayVersionNumber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Application Version : " + Application.version);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
